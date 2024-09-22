e77d73509d37acf48898c527ec47585a6614d81c
woltomeister <zsolt.varadi003@gmail.com>
2024-09-22 19:31:24 +0200

    HibaJavit

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];//hiba1
+    std::cout << '1-100 ertekek duplazasa'//hiba2
+    for (int i = 0;)//hiba3
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)//hiba4
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"//hiba5
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag0;//hiba6
+    for (int i = 0; i < N_ELEMENTS, i++)//hiba7
     {
-        atlag += b[i]
+        atlag += b[i]//hiba8
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
